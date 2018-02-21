2aa268a7fd23177167f1e37051d3e88a46c183ce
superchel <for.oleg.mozolev@gmail.com>
2018-02-21 15:34:16 +0200

    added production env configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,16 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.cache_store = :memory_store
 
+  @_cache_control = 'public, s-maxage=31536000, max-age=15552000' # heroku deflater var
+  cache_control = {
+    'Cache-Control' => 'public, s-maxage=31536000, max-age=15552000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
+  config.public_file_server.headers = cache_control
+
+  config.serve_static_assets = false
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
@@ -27,11 +36,12 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  # config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -48,11 +58,11 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :fatal
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -62,7 +72,21 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "so_land_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "siteon_#{Rails.env}"
+
+  # Example with Zoho mail server:
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.zoho.com',
+  #   port:                 587,
+  #   domain:               'siteon.com.ua',
+  #   user_name:            ENV['ZOHO_USERNAME'],
+  #   password:             ENV['ZOHO_PASS'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true  }
+
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
